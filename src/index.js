31d8d967f93319a40fad24f8f7e3210302167e5b
cristian.jora <cristian.jora@corebuild.eu>
2017-03-27 12:59:33 +0300

    Support plugin installation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-module.exports = {
-  VueTable: require("./components/Vuetable.vue"),
-  VueTablePagination: require("./components/VuetablePagination.vue"),
-  VueTablePaginationDropDown: require("./components/VuetablePaginationDropdown.vue"),
-  VueTablePaginationInfo: require("./components/VuetablePaginationInfo.vue"),
+import Vuetable from './components/Vuetable.vue'
+import VueTablePagination from './components/VuetablePagination.vue'
+import VueTablePaginationDropDown from './components/VuetablePaginationDropdown.vue'
+import VueTablePaginationInfo from './components/VuetablePaginationInfo.vue'
 
-  install(Vue) {
-    Vue.component("VueTable", module.exports.VueTable);
-    Vue.component("VueTablePagination", module.exports.VueTablePagination);
-    Vue.component("VueTablePaginationDropDown", module.exports.VueTablePaginationDropDown);
-    Vue.component("VueTablePaginationInfo", module.exports.VueTablePaginationInfo);
-  }
+function install(Vue){
+  Vue.component("vuetable", Vuetable);
+  Vue.component("vuetable-pagination", VueTablePaginationInfo);
+  Vue.component("vuetable-pagination-dropdown", VueTablePaginationDropDown);
+  Vue.component("vuetable-pagination-info", VueTablePaginationInfo);
+}
+export {
+  Vuetable,
+  VueTablePagination,
+  VueTablePaginationDropDown,
+  VueTablePaginationInfo,
+  install
 };
+
+export default Vuetable;
