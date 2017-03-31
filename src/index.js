dd4fe966e85a481863e384a67a7a6c51f8240731
cristian.jora <cristian.jora@corebuild.eu>
2017-03-31 12:45:54 +0300

    #10 Fix double registration of pagination info component Add build files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import VuetablePaginationInfo from './components/VuetablePaginationInfo.vue'
 
 function install(Vue){
   Vue.component("vuetable", Vuetable);
-  Vue.component("vuetable-pagination", VuetablePaginationInfo);
+  Vue.component("vuetable-pagination", VuetablePagination);
   Vue.component("vuetable-pagination-dropdown", VuetablePaginationDropDown);
   Vue.component("vuetable-pagination-info", VuetablePaginationInfo);
 }
