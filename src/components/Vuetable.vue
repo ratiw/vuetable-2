e4099ae2e2cb063fc0b7b8fe64c72c6f9b0c05b7
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-04-07 23:54:57 +0700

    Pass in sort field to components, close #68

--- src/components/Vuetable.vue
+++ src/components/Vuetable.vue
@@ -56,10 +56,14 @@
                     :checked="rowSelected(item, field.name)">
                 </td>
                 <td v-if="extractName(field.name) === '__component'" :class="['vuetable-component', field.dataClass]">
-                    <component :is="extractArgs(field.name)" :row-data="item" :row-index="index"></component>
+                  <component :is="extractArgs(field.name)"
+                    :row-data="item" :row-index="index" :row-field="field.sortField"
+                  ></component>
                 </td>
                 <td v-if="extractName(field.name) === '__slot'" :class="['vuetable-slot', field.dataClass]">
-                  <slot :name="extractArgs(field.name)" :row-data="item" :row-index="index"></slot>
+                  <slot :name="extractArgs(field.name)"
+                    :row-data="item" :row-index="index" :row-field="field.sortField"
+                  ></slot>
                 </td>
               </template>
               <template v-else>
