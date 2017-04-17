f761b2aa81b63ef323707bc611a9247efe8bc8f9
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-04-17 15:17:42 +0700

    move code into mixin

--- src/components/VuetablePaginationInfo.vue
+++ src/components/VuetablePaginationInfo.vue
@@ -9,27 +9,5 @@ import PaginationInfoMixin from './VuetablePaginationInfoMixin.vue'
 
 export default {
   mixins: [PaginationInfoMixin],
-  computed: {
-    paginationInfo () {
-      if (this.tablePagination == null || this.tablePagination.total == 0) {
-        return this.noDataTemplate
-      }
-
-      return this.infoTemplate
-        .replace('{from}', this.tablePagination.from || 0)
-        .replace('{to}', this.tablePagination.to || 0)
-        .replace('{total}', this.tablePagination.total || 0)
-    },
-  },
-  data: function() {
-    return {
-      tablePagination: null
-    }
-  },
-  methods: {
-    setPaginationData (tablePagination) {
-      this.tablePagination = tablePagination
-    },
-  },
 }
 </script>
--- src/components/VuetablePaginationInfoMixin.vue
+++ src/components/VuetablePaginationInfoMixin.vue
@@ -22,5 +22,30 @@ export default {
       }
     },
   },
+  data: function() {
+    return {
+      tablePagination: null
+    }
+  },
+  computed: {
+    paginationInfo () {
+      if (this.tablePagination == null || this.tablePagination.total == 0) {
+        return this.noDataTemplate
+      }
+
+      return this.infoTemplate
+        .replace('{from}', this.tablePagination.from || 0)
+        .replace('{to}', this.tablePagination.to || 0)
+        .replace('{total}', this.tablePagination.total || 0)
+    },
+  },
+  methods: {
+    setPaginationData (tablePagination) {
+      this.tablePagination = tablePagination
+    },
+    resetData () {
+      this.tablePagination = null
+    }
+  },
 }
 </script>
