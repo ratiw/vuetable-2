9c6a552c87153ca5fe5cd4fda834e79ed7c4d47e
Grzegorz Gąsak <vertoo.design@gmail.com>
2017-05-13 02:01:23 +0200

    Add option for info message on empty data

--- src/components/Vuetable.vue
+++ src/components/Vuetable.vue
@@ -96,6 +96,11 @@
           </tr>
         </template>
       </template>
+      <template v-if="countTableData === 0">
+        <tr>
+          <td :colspan="countVisibleFields" class="vuetable-empty-result">{{noDataTemplate}}</td>
+        </tr>
+      </template>
       <template v-if="lessThanMinRows">
         <tr v-for="i in blankRows" class="blank-row">
           <template v-for="field in tableFields">
@@ -236,7 +241,13 @@ export default {
     silent: {
       type: Boolean,
       default: false
-    }
+    },
+    noDataTemplate: {
+      type: String,
+      default() {
+        return 'No relevant data'
+      }
+    },
   },
   data () {
     return {
@@ -275,6 +286,12 @@ export default {
       return this.tableFields.filter(function(field) {
         return field.visible
       }).length
+    },
+    countTableData () {
+      if (this.tableData === null) {
+        return 0
+      }
+      return this.tableData.length
     },
     lessThanMinRows: function() {
       if (this.tableData === null || this.tableData.length === 0) {
@@ -873,5 +890,8 @@ export default {
   .vuetable-pagination-info {
     margin-top: auto;
     margin-bottom: auto;
+  }
+  .vuetable-empty-result {
+    text-align: center;
   }
 </style>
