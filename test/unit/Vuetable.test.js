aca5bc5316eec9772aa1582dc6add37d5b6cdecc
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-10-02 12:34:01 +0700

    snapshot test

--- test/unit/Vuetable.test.js
+++ test/unit/Vuetable.test.js
@@ -1,10 +1,58 @@
 import Vue from 'vue'
 import { mount, shallow } from 'vue-test-utils'
+import { createRenderer } from 'vue-server-renderer'
 import Vuetable from '@/components/Vuetable.vue'
 
 Vue.config.productionTip = false
 
 describe('Vuetable', () => {
+  it('has same basic HTML structure', () => {
+    const renderer = createRenderer()
+    const wrapper = shallow(Vuetable, {
+      propsData: {
+        fields: ['code', 'description'],
+        loadOnStart: false
+      }
+    })
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err)
+      expect(str).toMatchSnapshot()
+    })
+  })
+
+  it('has same HTML structure for __sequence special field', () => {
+    const renderer = createRenderer()
+    const wrapper = shallow(Vuetable, {
+      propsData: {
+        fields: ['__sequence', 'code'],
+        apiMode: false,
+        data: [
+          { code: 'foo' }
+        ]
+      }
+    })
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err)
+      expect(str).toMatchSnapshot()
+    })
+  })
+
+  it('has same HTML structure for __handle special field', () => {
+    const renderer = createRenderer()
+    const wrapper = shallow(Vuetable, {
+      propsData: {
+        fields: ['__handle', 'code'],
+        apiMode: false,
+        data: [
+          { code: 'foo' }
+        ]
+      }
+    })
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err)
+      expect(str).toMatchSnapshot()
+    })
+  })
 
   describe('Properties', () => {
     it('should have set the apiUrl correctly', () => {
