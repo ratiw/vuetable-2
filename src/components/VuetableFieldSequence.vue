53f3da00a68e9307baa12ec80be0f5eb322a0714
Rati Wannapanop <rati.wannapanop@gmail.com>
2018-01-06 10:17:18 +0700

    refactor sequence component

--- src/components/VuetableFieldSequence.vue
+++ src/components/VuetableFieldSequence.vue
@@ -8,11 +8,13 @@ import VuetableFieldMixin from './VuetableFieldMixin.vue'
 
 export default {
   name: 'vuetable-field-sequence',
+  
   mixins: [VuetableFieldMixin],
+
   methods: {
     renderSequence() {
-      return this.$parent.tablePagination
-        ? this.$parent.tablePagination.from + this.rowIndex
+      return this.vuetable.tablePagination
+        ? this.vuetable.tablePagination.from + this.rowIndex
         : this.rowIndex
     }
   }
