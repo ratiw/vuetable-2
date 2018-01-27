45ea5569376447c30cbf8054034772b4af9db35e
Rati Wannapanop <rati.wannapanop@gmail.com>
2018-01-27 16:03:01 +0700

    more tests

--- test/Vuetable.Methods.test.js
+++ test/Vuetable.Methods.test.js
@@ -96,13 +96,120 @@ describe('Vuetable - Methods', () => {
 
   describe('setData', () => {
 
-    it('emits loading and loaded events', () => {
+    it('emits loading and loaded events when argument is Array', () => {
       let wrapper = shallowVuetable(['code'])
-      wrapper.vm.setData([{code: 'AAA'}])
+      let data = [{code: 'AAA'}]
+      wrapper.vm.setData(data)
 
       let emitted = wrapper.emitted()
       expect(emitted).toHaveProperty('vuetable:loading')
       expect(emitted).toHaveProperty('vuetable:loaded')
+      expect(wrapper.vm.tableData).toBe(data)
+    })
+  })
+
+  describe('getAllQueryParams', () => {
+
+    const q1 = (sortOrder, currentPage, perPage) => {
+      // return object
+      return { sortOrder, currentPage, perPage }
+    }
+
+    const q2 = (sortOrder, currentPage, perPage) => {
+      // return something else which is not an object
+      return 'dummy'
+    }
+
+    const mountVuetable = (queryParams) => shallow(Vuetable, {
+      propsData: {
+        apiMode: false,
+        fields: ['code'],
+        queryParams
+      }
+    })
+
+    it('calls `query-params` if it is a Function', () => {
+      let wrapper = mountVuetable(q1)
+
+      expect(wrapper.vm.getAllQueryParams()).toEqual({
+        sortOrder: [], currentPage: 1, perPage: 10
+      })
+    })
+
+    it('returns empty object if the given Function does not return an Object', () => {
+      let wrapper = mountVuetable(q2)
+
+      expect(wrapper.vm.getAllQueryParams()).toEqual({})
+    })
+  })
+
+  describe('isSortable', () => {
+    
+    it('returns false if the given field did not define "sortField" option', () => {
+      let wrapper = shallowVuetable(['code'])
+      let field = wrapper.vm.tableFields[0]
+
+      expect(wrapper.vm.isSortable(field)).toEqual(false)
+    })
+
+    it('returns true if the given field defined "sortField" option', () => {
+      let wrapper = shallowVuetable([
+        { name: 'code', sortField: 'code' }
+      ])
+      let field = wrapper.vm.tableFields[0]
+
+      expect(wrapper.vm.isSortable(field)).toBe(true)
+    })    
+  })
+
+  describe('getObjectValue', () => {
+    let obj = { code: 'aaa' }
+    let cmp = shallowVuetable(['code']).vm
+
+    it('returns value inside the given object', () => {
+      expect(
+        cmp.getObjectValue(obj, 'code')
+      ).toEqual('aaa')
+    })
+
+    it('returns null when given path does not exist and no default value is given', () => {
+      expect(
+        cmp.getObjectValue(obj, 'foo')
+      ).toEqual(null)
+    })
+
+    it('returns default value when the given path does not exist and default value is given', () => {
+      expect(
+        cmp.getObjectValue(obj, 'foo', 'bar')
+      ).toEqual('bar')
+    })
+
+    it('returns the object when an empty path is given without default value', () => {
+      expect(
+        cmp.getObjectValue(obj, '')
+      ).toEqual(obj)
+    })
+
+    it('returns the object when an empty path is given eventhough the default value is given', () => {
+      expect(
+        cmp.getObjectValue(obj, '', 'foo')
+      ).toEqual(obj)
+    })
+
+    it('returns null when given object is null and no default value is given', () => {
+      expect(
+        cmp.getObjectValue(null, 'foo')
+      ).toEqual(null)
+
+      expect(
+        cmp.getObjectValue(null, '')
+      ).toEqual(null)
+    })
+
+    it('returns default value when given object is null and default value is given', () => {
+      expect(
+        cmp.getObjectValue(null, 'foo', 'baz')
+      ).toEqual('baz')
     })
   })
 })
