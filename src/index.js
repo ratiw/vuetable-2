a9100bc8988afb1f6d18920fcae67c57d78e3441
Rati Wannapanop <rati.wannapanop@gmail.com>
2018-04-25 11:30:03 +0700

    export and register additional components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import Vuetable from './components/Vuetable.vue'
+
+import VuetableFieldMixin from './components/VuetableFieldMixin.vue'
+import VuetableFieldCheckboxMixin from './components/VuetableFieldCheckboxMixin.vue'
+import VuetablePaginationMixin from './components/VuetablePaginationMixin.vue'
+import VuetablePaginationInfoMixin from './components/VuetablePaginationInfoMixin.vue'
+
 import VuetablePagination from './components/VuetablePagination.vue'
 import VuetablePaginationDropDown from './components/VuetablePaginationDropdown.vue'
 import VuetablePaginationInfo from './components/VuetablePaginationInfo.vue'
-import VuetablePaginationMixin from './components/VuetablePaginationMixin.vue'
-import VuetablePaginationInfoMixin from './components/VuetablePaginationInfoMixin.vue'
 import VuetableFieldCheckbox from './components/VuetableFieldCheckbox.vue'
 import VuetableFieldHandle from './components/VuetableFieldHandle.vue'
 import VuetableFieldSequence from './components/VuetableFieldSequence.vue'
 import VuetableRowHeader from './components/VuetableRowHeader.vue'
+import VuetableColGutter from './components/VuetableColGutter.vue'
 import Promise from 'promise-polyfill'
 
 if (!window.Promise) {
@@ -16,25 +21,32 @@ if (!window.Promise) {
 
 function install(Vue) {
   Vue.component("vuetable", Vuetable);
-  Vue.component("vuetable-pagination", VuetablePagination);
-  Vue.component("vuetable-pagination-dropdown", VuetablePaginationDropDown);
-  Vue.component("vuetable-pagination-info", VuetablePaginationInfo);
+  Vue.component("vuetable-col-gutter", VuetableColGutter)
   Vue.component("vuetable-field-checkbox", VuetableFieldCheckbox)
   Vue.component("vuetable-field-handle", VuetableFieldHandle)
   Vue.component("vuetable-field-sequence", VuetableFieldSequence)
+  Vue.component("vuetable-pagination", VuetablePagination);
+  Vue.component("vuetable-pagination-dropdown", VuetablePaginationDropDown);
+  Vue.component("vuetable-pagination-info", VuetablePaginationInfo);
   Vue.component("vuetable-row-header", VuetableRowHeader)
 }
 export {
   Vuetable,
-  VuetablePagination,
-  VuetablePaginationDropDown,
-  VuetablePaginationInfo,
-  VuetablePaginationMixin,
+  // Mixins
+  VuetableFieldMixin,
+  VuetableFieldCheckboxMixin,
   VuetablePaginationInfoMixin,
+  VuetablePaginationMixin,
+  // UI components
+  VuetableColGutter,
   VuetableFieldCheckbox,
   VuetableFieldHandle,
   VuetableFieldSequence,
+  VuetablePagination,
+  VuetablePaginationDropDown,
+  VuetablePaginationInfo,
   VuetableRowHeader,
+
   install
 };
 
